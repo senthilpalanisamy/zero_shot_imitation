865db323fb32f817923c720e802639c45db0ed78
Senthil Palanisamy <senthillihtnes1994@gmail.com>
2020-01-28 22:03:43 -0600

    Refined dataset reader to match pytorch input data format

--- inverse_model/dataset_reader.py
+++ inverse_model/dataset_reader.py
@@ -9,68 +9,91 @@ import cv2
 
 
 class BaxterPokingDataReader:
+
+  #TODO:Construct a visualization function to view the data
+  #TODO:Check if one hot encoding is necessary for the data
+  #TODO:Does storing as npy object make it faster to read data
   def __init__(self, base_path='./data'):
-    self._base_path = base_path
-    self._total_data = []
-    self._image_width = -1
-    self._image_height = -1
-    self._xbin_count = 20
-    self._ybin_count = 20
-    self._anglebin_count = 20
-    self._len_actionbin_count = 15
+    self.__base_path = base_path
+    self.total_data = []
+    self.__image_width = -1
+    self.__image_height = -1
+    self.__xbin_count = 20
+    self.__ybin_count = 20
+    self.__anglebin_count = 20
+    self.__len_actionbin_count = 16
+    self.__class_counts = [self.__xbin_count, self.__ybin_count,
+                          self.__anglebin_count, self.__len_actionbin_count]
  
-  def load_data_from_directory(self):
-    data_runs = os.listdir(self._base_path)
+  def __load_data_from_directory(self):
+    data_runs = os.listdir(self.__base_path)
     
     for folder in data_runs:
-      folder_path = os.path.join(self._base_path, folder)
+      folder_path = os.path.join(self.__base_path, folder)
       gt_file_path = os.path.join(folder_path, 'actions.npy')
       ground_truth = np.load(gt_file_path)
-      images_names = os.listdir(os.path.join(folder_path))
-      previous_image = cv2.imread(os.path.join(folder_path, images_names[0]))
-      for index, img_name in enumerate(images_names[1:]):
+      image_names = os.listdir(folder_path)
+      image_names = [file_name for file_name in image_names if file_name.endswith('.jpg')]
+      previous_image = cv2.imread(os.path.join(folder_path, image_names[0]))
+      
+      for index, img_name in enumerate(image_names[1:]):
         this_data = []
         current_image = cv2.imread(os.path.join(folder_path, img_name))
-        this_data.append([previous_image, current_image])
+        #print(ground_truth[index])
+        concat_image = np.concatenate((current_image[np.newaxis,: ], 
+                                       previous_image[np.newaxis, :]), axis=0)
+        concat_image = np.moveaxis(concat_image, -1, 1)
+        this_data.append(concat_image)
         this_data.append(ground_truth[index])
-        self._total_data.append(this_data)
+        self.total_data.append(this_data)
         previous_image = current_image
-    self._image_height = current_image.shape[0]
-    self._image_width = current_image.shape[1]
-    print(len(self._total_data))
+    self.__image_height = current_image.shape[0]
+    self.__image_width = current_image.shape[1]
+    print(len(self.total_data))
 
-  def remove_invalid_data(self):
+  def __remove_invalid_data(self):
     GT_INDEX=1
     IS_VALID_INDEX=-1
-    self._total_data = [data for data in self._total_data if 
+    self.total_data = [data for data in self.total_data if 
                         data[GT_INDEX][IS_VALID_INDEX]]
 
-  def dicretise_gt_actions(self):
+  def __dicretise_gt_actions(self):
     discretised_data = []
     ACTION_COORD_X=0
     ACTION_COORD_Y=1
     ANGLE=2
     ACTIONLEN=3
     TOTAL_ANGLE = 2 * np.pi
-    for data in self._total_data:
+    GT_TOTAL_NO = 4
+    for data in self.total_data:
       images, groundtruth = data
-      groundtruth[ACTION_COORD_X] =  round(groundtruth[ACTION_COORD_X] / self._image_width
-                                    * self._xbin_count)
-      groundtruth[ACTION_COORD_Y] =  round(groundtruth[ACTION_COORD_Y] / self._image_height
-                                    * self._ybin_count)
-      groundtruth[ANGLE] = round(groundtruth[ANGLE] / TOTAL_ANGLE * self._anglebin_count)
+      groundtruth[ACTION_COORD_X] =  round(groundtruth[ACTION_COORD_X] / self.__image_width
+                                    * (self.__xbin_count-1))
+      groundtruth[ACTION_COORD_Y] =  round(groundtruth[ACTION_COORD_Y] / self.__image_height
+                                    * (self.__ybin_count-1))
+      groundtruth[ANGLE] = round(groundtruth[ANGLE] / TOTAL_ANGLE * (self.__anglebin_count-1))
       groundtruth[ACTIONLEN] = round(groundtruth[ACTIONLEN] * 100)
-      discretised_data.append([images, groundtruth.astype(np.int8)])
-    self._total_data = discretised_data
+      groundtruth = groundtruth.astype(np.int8)
+      gt_onehot_vectors=[]
+      for index, gt_value in enumerate(groundtruth[:-1]):
+        #Construct a one-hot vector for ground truth
+        # print(self.__class_counts[index])
+        gt_onehot_vectors.append(np.eye(self.__class_counts[index])[gt_value])
+      #discretised_data.append([images, groundtruth.astype(np.int8)])
+      discretised_data.append([images, gt_onehot_vectors])
+    self.total_data = discretised_data
 
   def read_and_process_data(self):
-    self.load_data_from_directory()
-    self.remove_invalid_data()
-    self.dicretise_gt_actions()
+    self.__load_data_from_directory()
+    self.__remove_invalid_data()
+    self.__dicretise_gt_actions()
+
+  def get_stored_data(self):
+    return self.total_data
+   # How costly is having two copies of the same data
       
       
   
-  #TODO:Construct a visualization function to view the data
 
 if __name__=='__main__':
   data_directory  = '/home/senthilpalanisamy/work/winter_project/my_code/data/mini_data'
